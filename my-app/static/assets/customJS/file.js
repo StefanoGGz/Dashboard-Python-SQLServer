6608c17640ace6622cbbed4fabcd0f107da3b132
PracticanteAk <practicanteatacamakozan@gmail.com>
2025-03-19 17:25:49 -0300

    Fork 7
    <Descripción>
    -Eliminación de archivos y código innecesario.

--- my-app/static/assets/customJS/file.js
+++ my-app/static/assets/customJS/file.js
@@ -14,24 +14,3 @@ function readURL(input) {
 $("#imageUpload").change(function () {
   readURL(this);
 });
-
-/**
- * Formatear cantidad en salario
- */
-let salario = document.querySelector("#salario_empleado");
-if (salario) {
-  salario.addEventListener("input", (inputClick) => {
-    let cantidad = inputClick.target.value.replace(/\D/g, ""); // Eliminar caracteres no numéricos
-    cantidad = parseInt(cantidad, 10); // Convertir a número entero
-    if (isNaN(cantidad)) {
-      cantidad = 0; // Si no se puede convertir a número, se establece en 0
-    }
-    // Formatear la cantidad y asignarla al campo de entrada
-    inputClick.target.value =
-      "$ " +
-      cantidad.toLocaleString("es-CO", {
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 0,
-      });
-  });
-}
